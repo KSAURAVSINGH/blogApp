93daa4b778f5850b944d44bcdaf371e8e11889a0
raunak6662 <singhraunak402@gmail.com>
2023-01-04 10:53:33 +0530

    added register and login routes
    added register andlogin routes

--- app.js
+++ app.js
@@ -2,12 +2,42 @@ const mysql = require("mysql2");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { pool } = require("./db");
+const { pool1} = require("./db");
+const { response } = require("express");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("enter your userid and password");
 })
+
+app.post("/register", (req, res) => {
+    let email = req.body.email;
+    let passcode = req.body.passcode;
+
+    let sql = `INSERT INTO users (userID, passcode) VALUES (?, ?);`;
+    pool1.query(sql, [email, passcode], function(err, rows){
+        if(err) throw err;
+        res.send("Success");
+    })
+})
+
+app.post("/login", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.passcode;
+
+    let sql = `SELECT * FROM users where userID = ?;`;
+    pool1.query(sql, email, function(err, rows){
+        if(err) throw err;
+        let len = rows.length;
+        if(len == 1 && rows[0].passcode == password){
+            console.log("log in");
+        }else{
+            console.log("wrong");
+        }
+    })
+})
+
 // add new blog
 app.post("/blog", function(req, res){
     let title = req.body.title;
